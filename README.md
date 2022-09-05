3740af17862b489eb2d8ad72274a27984daf13cd
rafaelazizov <rafael.jr077@gmail.com>
2022-09-05 13:45:00 +0400

    nodejs

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # NodeJs_Sqlite_Sample_Backend
 Nodejs Backend Simple User model with sqlite connection
+
+Post Request:
+
+```
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "username": "user",
+  "email": "test@gmail.com",
+  "password": "p4ssword"
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("http://127.0.0.1:8000/user", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+// 1.Backend Module Load
+const express = require('express');
+
+// db1. Connect db.js
+const sequelize = require("./db");
+
+// Connect User Model
+const User = require('./User')
+
+// Sync connect db with npm server
+sequelize.sync().then( () => console.log("[+] Database Connection is OK") );
+
+// 2.Sever start
+const app = express();
+
+// db4. Serialize Json => Postmanda request atarken Post Request > Body > raw > Json > {include insert json}
+app.use(express.json());
+
+// 4.GET request
+app.get('/', (req, res) => {
+	res.send("Hellou baby");
+})
+
+// db3. Create POST request 
+app.post('/user', (req, res) => {
+	User.create(req.body).then(() => { res.send('User Created'); })
+})
+
+// 3.Listen port (port, callback)
+app.listen(8000, () => {
+	console.log('[+] Server is running 127.0.0.1:8000')
+});
+
+/*
+
+
+Serveri Hemise Save etdikden sonra yenide baslatmaq lazimdir
+normalda
+
+>>> node index.js
+
+Amma ozu daime reload atmasi ucun
+
+1.Install packege for auto start
+
+>>> yarn --dev add nodemon
+
+2.Open package.json 
+
+...
+"scripts": {
+    "start": "nodemon index.js"
+  },
+...
+
+3. Run Server
+>>> yarn start
+
+
+*/
+
+
+/*
+
+Sqlite3 connection
+
+1. Yarn install
+
+>>> yarn --dev add sqlite3
+>>> yarn --dev add sequelize
+
+2. Check package.json file 
+3. Create database.js
+
+*/
